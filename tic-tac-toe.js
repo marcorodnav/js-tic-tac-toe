85ef28dc73ea6f848561d720f603d89067ec7ba4
marcorodnav <marcorodnav@gmail.com>
2017-10-02 22:33:52 -0600

    added the grid

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,1 +1,30 @@
-console.log("here I am")
+const humanSymbol = "X"
+const computerSymbol = "O"
+const originalGrid = Array.from(Array(10).keys())
+const winingCombinations = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,4,8],
+  [2,4,6],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8]
+]
+
+const grid = document.querySelectorAll('.cell')
+
+const startGame = () => {
+  grid.forEach((cell) => {
+    cell.addEventListener('click', turnPlayer, false)
+  })
+}
+
+const resetButton = document.querySelector('#startButton');
+resetButton.addEventListener('click',startGame,false)
+const turnPlayer = (cell) => {
+  cell.target.innerText = humanSymbol
+}
+
+startGame()
+
