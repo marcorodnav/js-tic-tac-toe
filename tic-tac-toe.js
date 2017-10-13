530d69bded5986788667ab68fece1e00c26204b2
marcorodnav <marcorodnav@gmail.com>
2017-10-13 08:00:07 -0600

    add logic for playing

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,6 +1,6 @@
 const humanSymbol = "X"
 const computerSymbol = "O"
-const originalGrid = Array.from(Array(10).keys())
+let originalGrid = Array.from(Array(9).keys())
 const winingCombinations = [
   [0,1,2],
   [3,4,5],
@@ -14,17 +14,46 @@ const winingCombinations = [
 
 const grid = document.querySelectorAll('.cell')
 
+const checkTie = () => {
+  let cellsAvailables = originalGrid
+                              .filter((element) => !isNaN(element))
+                              .reduce((total, element) => total+element, 0)
+  if (cellsAvailables === 0) console.info("TIE!")
+}
+
+const playerTurn = (cell) => {
+  turn(cell.target.id, humanSymbol)
+}
 const startGame = () => {
   grid.forEach((cell) => {
+    cell.innerText = ""
+    originalGrid = Array.from(Array(9).keys())
     cell.addEventListener('click', playerTurn, false)
   })
 }
-
-const resetButton = document.querySelector('#startButton');
-resetButton.addEventListener('click',startGame,false)
-const playerTurn = (cell) => {
-  cell.target.innerText = humanSymbol
-}
-
 startGame()
 
+const resetButton = document.querySelector('#startButton')
+resetButton.addEventListener('click',startGame,false)
+
+const checkWin = (board, symbol) => {
+  let cellsPlayed = board.reduce((acc, elem, index) => (elem === symbol) ? acc.concat(index) : acc, [])
+  let gameWon;
+  for(let [index, winComb] in winingCombinations.entries()) {
+    if (winComb.every((element) => cellsPlayed.indexOf(element) > -1)) {
+      gameWon = {
+        index: index,
+        winner: symbol
+      }
+      break
+    }
+  }
+  return gameWon
+}
+
+const turn = (cellId, symbol) => {
+  originalGrid[cellId] = symbol
+  document.getElementById(cellId).innerText = symbol
+  // let gameWon = checkWin(originalGrid, symbol)
+  console.info(originalGrid)
+}
